b1b74424a0acb74e9df0f24889bf33605ed42a87
Leif Gensert <leif@leif.io>
2023-08-12 22:03:27 +0200

    Revert "use sveltekit"
    This reverts commit 07198b0c1b1c79462f07f265cfe716a56ca2b1ab.

--- .netlify/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.41d388db.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.6ea134b3.js"];
-export const stylesheets = [];
-export const fonts = [];
